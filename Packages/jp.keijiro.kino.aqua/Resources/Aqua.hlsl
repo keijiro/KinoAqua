2e3139a37a8eab16df63fbb49469a0c320242d6e
Keijiro Takahashi <keijiro@gmail.com>
2021-05-10 22:41:29 +0900

    Refactoring

--- Packages/jp.keijiro.kino.aqua/Resources/Aqua.hlsl
+++ Packages/jp.keijiro.kino.aqua/Resources/Aqua.hlsl
@@ -29,21 +29,36 @@ TEXTURE2D(_InputTexture);
 TEXTURE2D(_NoiseTexture);
 float _Opacity;
 
-float GetRandom(float2 p, float offs)
+float2 UV2SC(float2 uv)
 {
-    return GenerateHashedRandomFloat((p + offs + 2) * _ScreenParams.xy) * 2 - 1;
+    float2 p = uv - 0.5;
+    p.x *= _ScreenParams.x / _ScreenParams.y;
+    return p;
 }
 
-float GetNoise(float2 p, float offs)
+float2 SC2UV(float2 p)
 {
-    return SAMPLE_TEXTURE2D(_NoiseTexture, s_linear_repeat_sampler, p + offs).r;
+    p.x *= _ScreenParams.y / _ScreenParams.x;
+    return p + 0.5;
+}
+
+float2 RandomVector(float2 p)
+{
+    float2 res = _ScreenParams.xy;
+    float x = GenerateHashedRandomFloat((p + 10) * res);
+    float y = GenerateHashedRandomFloat((p + 20) * res);
+    return float2(x, y) * 2 - 1;
+}
+
+float GetNoise(float2 p)
+{
+    return SAMPLE_TEXTURE2D(_NoiseTexture, s_linear_repeat_sampler, p).r;
 }
 
 float3 SampleColor(float2 p)
 {
-    p.x *= _ScreenParams.y / _ScreenParams.x;
-    p += 0.5;
-    return SAMPLE_TEXTURE2D(_InputTexture, s_linear_clamp_sampler, p).rgb;
+    float2 uv = SC2UV(p);
+    return SAMPLE_TEXTURE2D(_InputTexture, s_linear_clamp_sampler, uv).rgb;
 }
 
 float SampleLuminance(float2 p)
@@ -57,9 +72,7 @@ float2 GetGradient(float2 p)
     const float2 dx = float2(1.0 / 400, 0);
     float ldx = SampleLuminance(p + dx.xy) - SampleLuminance(p - dx.xy);
     float ldy = SampleLuminance(p + dx.yx) - SampleLuminance(p - dx.yx);
-    ldx += GetRandom(p, 0) * 0.01;
-    ldy += GetRandom(p, 1) * 0.01;
-    return float2(ldx, ldy);
+    return float2(ldx, ldy) + RandomVector(p) / 100;
 }
 
 float2 Rotate90(float2 v)
@@ -67,12 +80,27 @@ float2 Rotate90(float2 v)
     return v.yx * float2(-1, 1);
 }
 
+float ProcessEdge(inout float2 p, float stride)
+{
+    float2 grad = GetGradient(p);
+    float edge = saturate(length(grad) * 10);
+    float bw = smoothstep(0.5, 0.7, GetNoise(p * 0.2));
+    p += normalize(Rotate90(grad)) * stride;
+    return lerp(1, bw, smoothstep(0.4, 1, edge));
+}
+
+float3 ProcessFill(inout float2 p, float stride)
+{
+    float2 grad = GetGradient(p);
+    p += normalize(grad) * stride;
+    return SampleColor(p);
+}
+
 float4 Fragment(Varyings input) : SV_Target
 {
     UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
 
-    float2 p = input.texcoord - 0.5;
-    p.x *= _ScreenParams.x / _ScreenParams.y;
+    float2 p = UV2SC(input.texcoord);
 
     float2 p_e_n = p;
     float2 p_e_p = p;
@@ -84,48 +112,20 @@ float4 Fragment(Varyings input) : SV_Target
 
     float  acc_e = 0;
     float3 acc_c = 0;
-
-    float sum_c = 0;
+    float  sum_c = 0;
 
     for (uint i = 0; i < Iteration; i++)
     {
-        {
-            float2 grad = GetGradient(p_e_n);
-            float edge = saturate(length(grad) * 10);
-            float bw = smoothstep(0.4, 0.6, GetNoise(p_e_n * 0.2, 0));
-            acc_e += lerp(1, bw, edge);
-            p_e_n -= normalize(Rotate90(grad)) * Stride * 2;
-        }
-
-        {
-            float2 grad = GetGradient(p_e_p);
-            float edge = saturate(length(grad) * 10);
-            float bw = smoothstep(0.4, 0.6, GetNoise(p_e_p * 0.2, 0));
-            acc_e += lerp(1, bw, edge);
-            p_e_p += normalize(Rotate90(grad)) * Stride * 2;
-        }
-
         float w_c = 0.5 + (float)i / Iteration;
-
-        {
-            float2 grad = GetGradient(p_c_n);
-            acc_c += SampleColor(p_c_n) * w_c;
-            sum_c += w_c;
-            p_c_n -= normalize(grad) * Stride;
-        }
-
-        w_c *= 0.3;
-
-        {
-            float2 grad = GetGradient(p_c_p);
-            acc_c += SampleColor(p_c_p) * w_c;
-            sum_c += w_c;
-            p_c_p += normalize(grad) * Stride;
-        }
+        acc_e += ProcessEdge(p_e_n, Stride * -2);
+        acc_e += ProcessEdge(p_e_p, Stride * +2);
+        acc_c += ProcessFill(p_c_n, -Stride) * w_c;
+        acc_c += ProcessFill(p_c_p, +Stride) * w_c * 0.3;
+        sum_c += w_c * 1.3;
     }
 
-    acc_c /= sum_c;
     acc_e /= Iteration * 2;
+    acc_c /= sum_c;
 
     return float4(acc_c * acc_e, 1);
 }
